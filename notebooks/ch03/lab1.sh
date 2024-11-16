760afd782e9b18aab3bb31d84a789205d8a20d0b
Tuturea Andrei <quilan@DESKTOP-RAR7IUP.>
2024-11-16 14:04:40 +0100

    Modifed notebook1 and 2

---
+++ notebooks/ch03/lab1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+
+# Export HOPSWORKS_API_KEY
+export HOPSWORKS_API_KEY=5koHpdzrA8PrEowf.3SFV5DYRwsEgXpiyVsxWlVhxNxcSSGjYTg4Xb76a9UuDv1hlT68pKgaLjRbjvMoA
+
+
+
+conda activate air_quality_env
+
+# Optional: Add any additional commands below
+echo "Conda environment 'air_quality_env' activated and API key exported!"
+
+
